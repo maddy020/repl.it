06bc74af3ab54b339349922e5a89b80e665f646e
maddy020 <madhavsetia24@gmail.com>
2026-01-22 14:01:41 +0530

    fixed session error

--- apps/web/app/provider.tsx
+++ apps/web/app/provider.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { getServerSession } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export async function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-    const session = await getServerSession(authOptions)
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+export function Providers({ children, session }: { children: React.ReactNode; session?: any }) {
+  return (
+    <SessionProvider session={session}>
+      {children}
+    </SessionProvider>
+  );
 }
